f32b20ff1002f235edb6e6b1a5c2ffc1e398e307
georgehua <hua.prsn@gmail.com>
2021-12-22 14:05:06 -0500

    Make a custom hook!

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 
 Following an Youtube tutorial: https://www.youtube.com/watch?v=j942wKiXFu8&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=1
+
+
+Tutorial Github: https://github.com/iamshaunjp/Complete-React-Tutorial/tree/main
--- george-blog/src/Home.js
+++ george-blog/src/Home.js
@@ -1,23 +1,10 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    return res.json()
-                })
-                .then(data => {
-                    setBlogs(data)
-                    setIsPending(false)
-                })
-        }, 1000)
-    }, [])
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
     return (
         <div className="home">
