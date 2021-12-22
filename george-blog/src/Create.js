e528a474cf26a8e84ea24ed82a432d6cf63bd1cb
georgehua <hua.prsn@gmail.com>
2021-12-22 17:38:48 -0500

    delete blogs

--- george-blog/src/Create.js
+++ george-blog/src/Create.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
 
@@ -6,6 +7,8 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false)
+    const history = useHistory();
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,7 +23,10 @@ const Create = () => {
         }).then(() => {
             console.log("new blog added")
             setIsPending(false)
+            history.push('/')
         })
+
+
     }
     return (
         <div className="create">
