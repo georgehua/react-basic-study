75d3011794723267a00b0a8f96783f70ed8dad09
georgehua <hua.prsn@gmail.com>
2021-12-22 12:55:03 -0500

    useEffect
    This is like watch functionality for Vue

--- george-blog/src/Home.js
+++ george-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -9,10 +9,18 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id != id)
+        setBlogs(newBlogs)
+    };
+
+    useEffect(() => {
+        console.log("use effect run")
+    })
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
         </div>
     );
 }
