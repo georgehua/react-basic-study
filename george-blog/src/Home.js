125a0b5b6b63bd5c1396e90f8f1586c5d48802bf
georgehua <hua.prsn@gmail.com>
2021-12-22 13:07:13 -0500

    json server, fetch data and checking null

--- george-blog/src/Home.js
+++ george-blog/src/Home.js
@@ -3,11 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id != id)
@@ -15,12 +11,18 @@ const Home = () => {
     };
 
     useEffect(() => {
-        console.log("use effect run")
-    })
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data)
+            })
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
         </div>
     );
 }
